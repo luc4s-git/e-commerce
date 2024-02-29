90dca58cbc0eb052e977a77fd51a77205e8c30a5
luc4s-git <lucassantos4332@hotmail.com>
2024-02-29 13:32:15 -0300

    add: 404 and default error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,3 +1,35 @@
+import { Link, useRouteError } from 'react-router-dom';
+
 export default function Error() {
-  return <div>Error</div>;
+  const error = useRouteError();
+
+  if (error.status === 404) {
+    console.log(error);
+
+    return (
+      <main className="grid place-items-center min-h-[100vh]">
+        <div className="text-center">
+          <p className="text-9xl font-semibold text-primary">{error.status}</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
+            {error.statusText}
+          </h1>
+          <p className="mt-6 text-lg leading-6">
+            {"Sorry, we weren't able to find the page you are looking for."}
+          </p>
+          <div className="mt-10">
+            <Link to={'/'} className="btn btn-secondary capitalize">
+              go back home
+            </Link>
+          </div>
+        </div>
+      </main>
+    );
+  }
+
+  console.log(error);
+  return (
+    <main className="grid min-h-[100vh] place-items-center">
+      Sorry, there was an error.
+    </main>
+  );
 }
