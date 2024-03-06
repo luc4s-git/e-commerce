9384e52829d2b3d587330f9b47e5f857cf23ef2f
luc4s-git <lucassantos4332@hotmail.com>
2024-03-06 17:37:58 -0300

    add: PaginationContainer and functionality

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -1,3 +1,63 @@
+import { useLoaderData, useLocation, useNavigate } from 'react-router-dom';
+
 export default function PaginationContainer() {
-  return <div>PaginationContainer</div>;
+  const { meta } = useLoaderData();
+
+  const { page, pageCount } = meta.pagination;
+
+  const pages = Array.from({ length: pageCount }, (_, index) => {
+    return index + 1;
+  });
+
+  const { search, pathname } = useLocation();
+  console.log(search);
+  const navigate = useNavigate();
+
+  const handlePageChange = (pageNumber) => {
+    const searchParams = new URLSearchParams(search);
+    searchParams.set('page', pageNumber);
+    navigate(`${pathname}?${searchParams.toString()}`);
+  };
+
+  return (
+    <div className="mt-16 flex justify-end">
+      <div className="join">
+        <button
+          onClick={() => {
+            let previousPage = page - 1;
+            if (previousPage < 1) previousPage = pageCount;
+            handlePageChange(previousPage);
+          }}
+          className="btn btn-xs sm:btn-md join-item uppercase"
+        >
+          prev
+        </button>
+        {pages.map((item, index) => {
+          return (
+            <button
+              onClick={() => {
+                handlePageChange(item);
+              }}
+              key={index}
+              className={`btn btn-xs sm:btn-md border-none join-item ${
+                page === item && 'bg-base-300 border-base-300'
+              }`}
+            >
+              {item}
+            </button>
+          );
+        })}
+        <button
+          onClick={() => {
+            let nextPage = page + 1;
+            if (nextPage > pageCount) nextPage = 1;
+            handlePageChange(nextPage);
+          }}
+          className="btn btn-xs sm:btn-md join-item uppercase"
+        >
+          next
+        </button>
+      </div>
+    </div>
+  );
 }
