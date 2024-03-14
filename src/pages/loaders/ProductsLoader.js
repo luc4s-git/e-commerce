c33e5eb525a575f56102f04e539ea47e39daa336
luc4s-git <lucassantos4332@hotmail.com>
2024-03-14 19:49:37 -0300

    add: all ProductsLoader

--- src/pages/loaders/ProductsLoader.js
+++ src/pages/loaders/ProductsLoader.js
@@ -2,6 +2,25 @@ import { instance } from '../../utils';
 
 const url = '/products';
 
+const allProductsQuery = (queryParams) => {
+  const { search, category, company, sort, price, shipping, page } =
+    queryParams;
+
+  return {
+    queryKey: [
+      'products',
+      search ?? '',
+      category ?? 'all',
+      company ?? 'all',
+      sort ?? 'a-z',
+      price ?? 100000,
+      shipping ?? false,
+      page ?? 1,
+    ],
+    queryFn: () => instance(url, { params: queryParams }),
+  };
+};
+
 export const loader =
   (queryClient) =>
   async ({ request }) => {
@@ -9,7 +28,9 @@ export const loader =
       ...new URL(request.url).searchParams.entries(),
     ]);
 
-    const response = await instance(url, { params });
+    const response = await queryClient.ensureQueryData(
+      allProductsQuery(params)
+    );
 
     const products = response.data.data;
     const meta = response.data.meta;
